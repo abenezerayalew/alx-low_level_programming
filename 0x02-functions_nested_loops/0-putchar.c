c8a1b63af6df6fd3f44fad12b81f86d3d5c664e8
abenezerayalew <aben.man.unity@gmail.com>
2022-09-13 10:54:41 -0700

    create

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<studio.h>
+/*
+ * main - Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: Always 0 (Success) 
+ */
+int main(void)
+{
+	putchar("prints _putchar");
+        return (0);
+}
+
