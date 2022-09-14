3e006d89570ec1daebd76e7b5bcd8c2529d72e6c
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-09-14 06:35:32 +0300

    create

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,76 @@
+
+Skip to content
+iAmG-r00t /
+alx-low_level_programming
+Public
+
+Code
+Issues 1
+Pull requests
+Actions
+Projects
+
+More
+alx-low_level_programming/0x02-functions_nested_loops/100-times_table.c
+@iAmG-r00t
+iAmG-r00t fix code logic, minor edit.. 🐙😀😭👌
+History
+1 contributor
+41 lines (35 sloc) 660 Bytes
+#include"main.h"
+
+/**
+ * print_times_table - prints time table of n
+ *
+ * @n: takes number input
+*/
+
+void print_times_table(int n)
+{
+	int prod, mult, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
