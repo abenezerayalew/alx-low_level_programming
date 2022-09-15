39df169aa3a69fc41edc3869a911146492c5c131
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-09-15 16:22:16 +0300

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 /**
- * _isupper - check if c is upper
+ * _isupper - check if c is upper by usin ascii code
  *
- * @c: input for alphabet
+ * @c: input for alphabet 
  *
- * Return: 1 if its uppercase, 0 if not
+ * Return: 1 if its uppercase, 0 else
 */
-int _isupper(int c){
-if (c >= 65 && c <= 90)
-  return(1);
-  else
-    return(0);
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
