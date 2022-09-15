9b4b464fc49a4826ec33dc21dcaa3d7a9ff7e096
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-09-15 19:03:29 +0300

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,21 @@
 
 void more_numbers(void)
 {
-  int i=0,j=0;
-while(i<=10)
-{
-  j=0;
-  while(j<=14)
-  {
-    _putchar(i);
-    j++;
-  }
-  _putchar("\n");
-  i++;
+	int num, i, j;
+
+	for (i = 1; i <= 10; ++i)
+	{
+		for (j = 0; j <= 14; ++j)
+		{
+			num = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				num = j % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
 }
-}
+
