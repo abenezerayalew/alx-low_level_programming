4a5dd4b644038ba100f00ea404877fd4c65b2117
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-09-28 17:30:09 +0300

    update

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,16 @@
 /**
-* factorial - a function that returns the factorial of a given number
-*
-* @n: positive number
-*
-* return: factorial of @n
+ * factorial - a function that returns the factorial of a given number
+ *
+ * @n: positive number
+ *
+ * Return: factorial of @n
 */
-
 int factorial(int n)
 {
-  if ( n < 0 )
-    return (-1); /* -1 to indicate an error */
-  else if ( n == 0)
-    return (1); /*return factorial of 0 */
-  else
-    return (n*factorial(n-1); /*return factorial of n if its not less or equal to 0*/
+	if (n < 0)
+		return (-1);	/* -1 to indicate an error */
+	else if (n == 0)
+		return (1);	/*return factorial of 0 */
+	else
+		return (n * factorial(n - 1));	/*return factorial of n if its not less or equal to 0*/
 }
