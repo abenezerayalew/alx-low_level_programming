d69186bb08bc7fff8b181280d65bfb9f5b135493
abenezerayalew <aben.man.unity@gmail.com>
2022-09-30 05:33:19 -0700

    3rd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * main - a program that prints all arguments it receives
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("error");
+		return (1);
+	}
+	else
+	{
+	int i=1,mul=1;
+	mul = (atoi(argv[i]) * atoi(argv[2]));
+	printf("%d\n",mul);
+	}
+	return (0);
+}
