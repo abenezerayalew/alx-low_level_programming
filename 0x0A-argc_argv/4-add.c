e991d52aa8588b783bd519c104bdfd97c7df5500
abenezerayalew <aben.man.unity@gmail.com>
2022-09-30 06:49:48 -0700

    last

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,41 +3,38 @@
 
 /**
  * isNumber - check if string array is number
- * @num: string to check
- * Return: 0 if it's a number
- *         1 if it's not a number
+ * @s: string to check
+ *
+ * Return: 0 or 1
 */
 
-
-int isNumber(char num[])
+int isNumber(const char *s)
 {
-	int i, l = strlen(num);
+	int i = 0;
 
-	for (i = 0; i < l; i++)
+	while (s[i] != '\0')
 	{
-		if (!isdigit(num[i]))
-			return (1);
+		if (s[i] < '0' || s[i] > '9')
+		return (1);
+		i++;
 	}
 	return (0);
 }
 
-
 /**
  * main - a program that adds positive numbers
- *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 
 int main(int argc, char const *argv[])
 {
 int sum = 0;
 while (--argc)
 {
-	if (isInteger(argv[argc]))
+	if (isNumber(argv[argc]))
 	{
 		printf("Error\n");
 		return (1);
