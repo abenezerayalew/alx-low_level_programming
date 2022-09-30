f506ba15e5afa6357675bc6bf5d6b7a5121d6c1f
abenezerayalew <aben.man.unity@gmail.com>
2022-09-30 03:17:41 -0700

    first challenge git push q!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  a program that prints its name, followed by a new line
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((unused))  argc,char *argv[])
+{
+	int i = 0;
+	printf("%s\n", argv[i]);
+	return (0);
+}
