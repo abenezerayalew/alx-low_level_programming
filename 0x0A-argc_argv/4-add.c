abeb8db687664d2fdc9ff21f80fd61727203f050
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-09-30 16:25:27 +0300

    4th

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * isNumber - check if string array is number
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+*/
+
+
+int isNumber(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0; i < l; i++)
+	{
+		if (!isdigit(num[i]))
+			return (1);
+	}
+	return (0);
+}
+
+
+/**
+ * main - a program that adds positive numbers
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(int argc, char const *argv[])
+{
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum += atoi(argv[argc]);
+}
+
+printf("%i\n", sum);
+
+return (0);
+}
