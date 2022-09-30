3b7d032d494719e38d06ea8ffdbc6e903c1172b3
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-09-30 15:46:18 +0300

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints all arguments it receives
+ * main - a program that multiplies two arguments passed into it
  *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(int argc, char *argv[])
 {
-	int  mul;
+	int m;
 
 	if (argc != 3)
 	{
-		printf("error");
+		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		mul = (atoi(argv[1]) * atoi(argv[2]));
-		printf("%d\n", mul);
+		m = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", m);
 	}
 	return (0);
 }
