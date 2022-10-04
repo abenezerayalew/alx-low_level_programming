a2ec9759a42b2771feab321925f093f7d0ac846c
abenezerayalew <aben.man.unity@gmail.com>
2022-10-04 13:54:09 -0700

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,33 +6,27 @@
  * @height: int
  * Return: 2d array
  */
-
 int **alloc_grid(int width, int height)
 {
-int **tab, i, j;
-tab = malloc(sizeof(*tab) * height);
+	int **tab, i, j;
 
-if (width <= 0 || height <= 0 || tab == 0)
-	return (NULL);
+	tab = malloc(sizeof(*tab) * height);
+	if (width <= 0 || height <= 0 || tab == 0)
+		return (NULL);
 
-else
-{
-	for (i = 0; i < height; i++)
-	{
-		tab[i] = malloc(sizeof(**tab) * width);
-		if (tab[i] == 0)
+		for (i = 0; i < height; i++)
 		{
-			/*Free everything if malloc fails*/
-			while (i--)
-				free(tab[i]);
-			free(tab);
-			return (NULL);
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
+			{
+				/*Free everything if malloc fails*/
+				while (i--)
+					free(tab[i]);
+				free(tab);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
 		}
-
-		for (j = 0; j < width; j++)
-			tab[i][j] = 0;
 	}
-}
 
-return (tab);
-}
