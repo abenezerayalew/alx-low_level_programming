75681ec71ae38910d4a06c794062efc56ea06826
abenezerayalew <aben.man.unity@gmail.com>
2022-10-04 10:49:16 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,10 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *s;
+	char *s = malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || s == 0)
 		return (NULL);
-	s = malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 		s[i] = c;
