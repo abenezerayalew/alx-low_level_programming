7275bbc7f625388f134558b9b76dbd5521e449b0
abenezerayalew <aben.man.unity@gmail.com>
2022-10-06 07:30:47 -0700

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,11 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	char *pr = s;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+	while (n--)
+		*s++ = b;
+	return (pr);
 }
 
 /**
