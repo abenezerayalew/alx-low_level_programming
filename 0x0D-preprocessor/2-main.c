9a517632af519adfad231bf82eb9ec877bbce1af
abenezer_ayalew <44866028+abenezerayalew@users.noreply.github.com>
2022-10-10 23:09:39 +0300

    fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the name of the file it was compiled
- * return: Always 0 (Success)
- */
+ * main - Entry point
+ *
+ * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
