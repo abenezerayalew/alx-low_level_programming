8ce8cbc05ff1df65c925f870bf3438bd226876ad
abenezerayalew <aben.man.unity@gmail.com>
2022-10-13 12:08:41 -0700

    update

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  a function that prints numbers, followed by a new line
+ *
+ * @n: variable start
+ * *separator: is the string to be printed between numbers
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, print;
+	va_list(arg);
+	va_start(arg, n);
+	for(i=0; i < n; i++)
+	{
+	print = va_arg(arg, int);
+	printf("%d", print);
+	if (separator && i != n - 1)
+	printf("%s", separator);
+	
+	}
+	printf("\n");
+}
