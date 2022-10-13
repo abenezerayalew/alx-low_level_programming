428950ef03c63873c4450ddc5f4b84fd32b841bd
abenezerayalew <aben.man.unity@gmail.com>
2022-10-13 13:02:39 -0700

    3rd

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - a function that prints strings, followed by a new line.
+ *
+ * @separator: pointer to constant separator
+ * @n: start of input variables
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+
+	va_list(arg);
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(arg, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nill)");
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(arg);
+	printf("\n");
+}
