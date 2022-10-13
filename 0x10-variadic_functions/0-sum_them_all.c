7e02d34a6f9a04c629869d5fe5b2cbe4459caf02
abenezerayalew <aben.man.unity@gmail.com>
2022-10-13 10:45:57 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,1 +1,22 @@
+#include "variadic_functions.h"
 
+/**
+ * sum_them_all - a function that returns the sum of all its parameters
+ *
+ * @n:arg
+ *
+ * Return: sum (Success)
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+
+	va_list(arg);
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+	sum = sum + va_arg(arg, int);
+	}
+	return (sum);
+	va_end(arg);
+}
