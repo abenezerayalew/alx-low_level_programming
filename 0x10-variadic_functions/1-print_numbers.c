a489c9a5af23e97dda7a4ed3e3f50ae2204acd8e
abenezerayalew <aben.man.unity@gmail.com>
2022-10-13 12:13:17 -0700

    fix betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
  * print_numbers -  a function that prints numbers, followed by a new line
  *
  * @n: variable start
- * *separator: is the string to be printed between numbers
+ * @separator: is the string to be printed between numbers
  *
  * Return: nothing
  */
@@ -12,15 +12,15 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i, print;
+
 	va_list(arg);
 	va_start(arg, n);
-	for(i=0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 	print = va_arg(arg, int);
 	printf("%d", print);
 	if (separator && i != n - 1)
 	printf("%s", separator);
-	
 	}
 	printf("\n");
 }
