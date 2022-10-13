0dff8d688a6b5695af9be63e19d3d20844800f15
abenezerayalew <aben.man.unity@gmail.com>
2022-10-13 13:27:36 -0700

    create

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - a function that prints anything
+ *
+ * @format: a list of argument pointing
+ *
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	
