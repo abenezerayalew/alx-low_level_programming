5068e3c5700b34ef48d0aadb85f1f41617a97a7b
abenezerayalew <aben.man.unity@gmail.com>
2022-10-20 08:11:04 -0700

    advs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+nclude "lists.h"
+/**
+*premain - function that run before main
+*
+* Return: nothing
+*/
+void __attribute__ ((constructor)) premain()
+{
+printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+}
