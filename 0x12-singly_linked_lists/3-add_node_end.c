302558ca3968d85a362fd494c73c8f63a115a0b3
abenezerayalew <aben.man.unity@gmail.com>
2022-10-20 07:57:42 -0700

    fix-betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *temp;
 	size_t str_len = 0;
 
-	
 	if (str == NULL)
 		str_len = 0;
 
@@ -29,15 +28,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = str_len;
 	new_node->next = NULL;
 
-	
 	temp = *head;
 
-	
 	if (temp == NULL)
 		*head = new_node;
 	else
 	{
-	
 		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new_node;
