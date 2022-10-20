04cd9d44087e3f18a81b03b78be0349d7a1f5358
abenezerayalew <aben.man.unity@gmail.com>
2022-10-20 06:28:02 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,17 +12,21 @@ size_t print_list(const list_t *h)
 {
 	size_t counter = 1;
 
+	if (h == NULL)
+		return (0);
+
 	while (h->next != NULL)
 	{
-		/* If str is NULL, print [0] (nil)  */
+		/* If str is NULL, return len (0) and str  (nil)  */
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-
+		/* number of node  */
 		counter += 1;
 	}
+	/*print thee last node*/
 	printf("[%d] %s\n", h->len, h->str);
 
 	return (counter);
