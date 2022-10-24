0241123fdcbc46d5596a98edd06997a78ae2989c
abenezerayalew <aben.man.unity@gmail.com>
2022-10-24 06:12:29 -0700

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * listint_len - a function that returns the number of elements
+ *
+ * @h: point to the 1st node
+ *
+ * Return: the number of elements
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t counter = 1;
