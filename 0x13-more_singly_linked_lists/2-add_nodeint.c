5e24a49631b88fbdf81e3b6342bb846501da5ff1
abenezerayalew <aben.man.unity@gmail.com>
2022-10-25 05:40:59 -0700

    update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,12 +13,15 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = malloc(sizeof(listint_t));
+	new = (listint_t *)malloc(sizeof(listint_t));
 
 	if (new == NULL)
-	return (NULL);
-
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = n;
+
 	new->next = *head;
 	*head = new;
 
