0cd7cd898a675d1c22a24d2cec16638e11420516
abenezerayalew <aben.man.unity@gmail.com>
2022-10-25 03:27:45 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,12 +12,11 @@ size_t print_listint(const listint_t *h)
 
 	if (h == NULL)
 	return (0);
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
-	printf("%d\n", h->n);
 	return (count);
 }
