6c62789fcf3d639852158f2c645032785b8cc73e
abenezerayalew <aben.man.unity@gmail.com>
2022-10-25 03:11:49 -0700

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,17 +11,21 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *ptr, *new;
+
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
+	new->n = n;
+	new->next = NULL;
 	ptr = *head;
-	new = (listint_t*)malloc(sizeof(listint_t));
-	new->n=n;
-	new->next=NULL;
-
+	if (*head == NULL)
+		*head = new;
+	else
+	{
 	while (ptr->next != NULL)
 		ptr = ptr->next;
-	ptr->next=new;
-
+	ptr->next = new;
+	}
 	return (*head);
 }
