56b14a146828b49d0df059b7d57e17f853cd3640
abenezerayalew <aben.man.unity@gmail.com>
2022-10-25 01:56:16 -0700

    3rd

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/*
+ * add_nodeint_end - a function that adds a new node at the end of a list
+ *
+ * @head: point to the 1st node
+ * @n: new data
+ *
+ * REturn: the address of the new element, or NULL if it failled
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *new;
+	ptr = *head;
+	new = (listint_t*)malloc(sizeof(listint_t));
+	new->n=n;
+	new->next=NULL;
+
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next=new;
+	return (*head);
+}
