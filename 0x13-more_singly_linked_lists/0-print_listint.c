aa7d48f3bcbadfaaa81586e8070710a501093848
abenezerayalew <aben.man.unity@gmail.com>
2022-10-25 00:48:13 -0700

    l_update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 	size_t count = 1;
 
 	if (h->next == NULL)
-	printf("Linked List is empty :(");
+	return (0);
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
