d06b71f8cb7c791352c15b586bd2c5698c75a9c7
abenezerayalew <aben.man.unity@gmail.com>
2023-04-11 14:20:38 +0300

    error_not_found

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,27 +3,36 @@
 /**
 * linear_search - searches for a value in a array.
 *
+* Description: linear_search function for a value in an array of
+* integers using the Linear search algorithm.
+*
 * @array: A pointer of the array
 * @size: The num of elements in the array
 * @value: The value to search form array
 * Return: if a match is not found return -1
-*
-* Description: linear_search function for a value in an array of
-* integers using the Linear search algorithm.
 */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
+	int found;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
-	for (index = 0; index < size; index++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-		if (array[index] == value)
-			return (index);
-	}
-	return (-1);
-}
+	index = 0;
+	found = 0;
 
+	do {
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		if (value == array[index])
+		{
+			found = 1;
+			return (index);
+		}
+		else
+			index++;
+	} while (found == 0 && index < size);
+	if (found == 0)
+		index = -1;
+	return (index);
+}
