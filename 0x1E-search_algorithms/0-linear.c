ed8e67632eaf44902d71f6fe21d62b6d54484f6e
abenezerayalew <aben.man.unity@gmail.com>
2023-04-11 14:16:36 +0300

    test_finding_error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,11 +16,11 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 	for (index = 0; index < size; index++)
 	{
-		printf("Value checked array[%li] = [%i]\n", index, array[index]);
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 		if (array[index] == value)
 			return (index);
 	}
